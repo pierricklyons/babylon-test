952bf469843a2f7a170ff332d223c7e25427ae7d
pierr <pierrick@live.co.uk>
2023-04-24 18:56:52 +0100

    Textures and skybox

--- src/App.tsx
+++ src/App.tsx
@@ -1,55 +1,12 @@
-import React from "react";
-import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Mesh, Scene } from "@babylonjs/core";
-import SceneComponent from "./babylon-components/SceneComponent"; // import SceneComponent
+// import React from "react";
+// import {BasicScene} from "./babylon-components/BasicScene"
+// import {StandardMaterialScene} from "./babylon-components/StandardMaterialScene"
+import {PBRMaterialScene} from "./babylon-components/PBRMaterialScene"
 // import SceneComponent from 'babylonjs-hook'; // if you install 'babylonjs-hook' NPM.
 import "./App.css";
 
-let box : Mesh | undefined;
-let ball : Mesh | undefined;
-
-const onSceneReady = (scene : Scene) => {
-	// This creates and positions a free camera (non-mesh)
-	const camera = new FreeCamera("camera1", new Vector3(0, 10, -10), scene);
-
-	// This targets the camera to scene origin
-	camera.setTarget(Vector3.Zero());
-
-	const canvas = scene.getEngine().getRenderingCanvas();
-
-	// This attaches the camera to the canvas
-	camera.attachControl(canvas, true);
-
-	// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-	const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
-	// Default intensity is 1. Let's dim the light a small amount
-	light.intensity = .7;
-
-	// Our built-in 'box' shape.
-	box = MeshBuilder.CreateBox("box", { size: 2 }, scene);
-	// Move the box upward 1/2 its height
-	box.position.y = 1;
-
-	ball = MeshBuilder.CreateSphere("ball", {diameter: 2}, scene);
-	ball.position.y = 3.5;
-
-	// Our built-in 'ground' shape.
-	MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-};
-
-/**
- * Will run on every frame render.  We are spinning the box on y-axis.
- */
-const onRender = (scene : Scene) => {
-	if (box !== undefined) {
-		const deltaTimeInMillis = scene.getEngine().getDeltaTime();
-
-		const rpm = 10;
-		box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 1000);
-	}
-};
+const scene = new PBRMaterialScene()
 
 export default () => (
-	<div>
-		<SceneComponent antialias onSceneReady={onSceneReady} onRender={onRender} id="my-canvas" />
-	</div>
+	scene.createScene()
 );
---
+++ src/babylon-components/StandardMaterialScene.tsx
@@ -0,0 +1,96 @@
+import SceneComponent from "./SceneComponent"; // import SceneComponent
+import { FreeCamera, Vector3, HemisphericLight, MeshBuilder, Mesh, Scene, StandardMaterial, Texture} from "@babylonjs/core";
+
+export class StandardMaterialScene {
+    private createGroundMaterial(scene : Scene) : StandardMaterial {
+        const groundMaterial = new StandardMaterial("groundMaterial", scene);
+
+		const uvScale = 4;
+		const textures : Texture[] = []
+
+		const diffuse = new Texture("/textures/stone/stone-diffuse.jpg", scene);
+		const normal = new Texture("/textures/stone/stone-normal.jpg", scene);
+		const ao = new Texture("/textures/stone/stone-ao.jpg", scene);
+
+		groundMaterial.diffuseTexture = diffuse;
+		groundMaterial.bumpTexture = normal;
+		groundMaterial.ambientTexture = ao;
+
+		textures.push(diffuse, normal, ao);
+
+		textures.forEach((texture) => {
+			texture.uScale = uvScale;
+			texture.vScale = uvScale;
+		})
+		
+		return groundMaterial
+    }
+
+    private createBallMaterial(scene : Scene) : StandardMaterial {
+        const ballMaterial = new StandardMaterial("ballMaterial", scene);
+
+		const uvScale = 2;
+		const textures : Texture[] = []
+
+		const diffuse = new Texture("/textures/metal/metal-diffuse.jpg", scene);
+		ballMaterial.diffuseTexture = diffuse;
+
+		const normal = new Texture("/textures/metal/metal-normal.jpg", scene);
+		ballMaterial.bumpTexture = normal;
+		ballMaterial.invertNormalMapX = true;
+		ballMaterial.invertNormalMapY = true;
+
+		const ao = new Texture("/textures/metal/metal-ao.jpg", scene);
+		ballMaterial.ambientTexture = ao;
+
+		const specular = new Texture("/textures/metal/metal-ao.jpg", scene);
+		ballMaterial.specularTexture = specular;
+
+		textures.push(diffuse, normal, ao, specular);
+
+		textures.forEach((texture) => {
+			texture.uScale = uvScale;
+			texture.vScale = uvScale;
+		})
+		
+		return ballMaterial
+    }
+
+	ball : Mesh | undefined;
+    ground : Mesh | undefined;
+
+	private onSceneReady = (scene : Scene) => {
+		// This creates and positions a free camera (non-mesh)
+		const camera = new FreeCamera("camera1", new Vector3(0, 10, -10), scene);
+	
+		// This targets the camera to scene origin
+		camera.setTarget(Vector3.Zero());
+	
+		const canvas = scene.getEngine().getRenderingCanvas();
+	
+		// This attaches the camera to the canvas
+		camera.attachControl(canvas, true);
+	
+		// This creates a light, aiming 0,1,0 - to the sky (non-mesh)
+		const light = new HemisphericLight("light", new Vector3(0, 1, 0), scene);
+		// Default intensity is 1. Let's dim the light a small amount
+		light.intensity = 1;
+	
+		this.ground = MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
+        this.ground.material = this.createGroundMaterial(scene)
+
+        this.ball = MeshBuilder.CreateSphere("ball", {diameter: 2}, scene);
+		this.ball.position.y = 1;
+		this.ball.material = this.createBallMaterial(scene)
+	};
+	
+	/**
+	 * Will run on every frame render.  We are spinning the box on y-axis.
+	 */
+	private onRender = (scene : Scene) => {};
+
+	public createScene = () => {
+		return <div><SceneComponent antialias onSceneReady={this.onSceneReady} onRender={this.onRender} id="my-canvas" /></div>
+	}
+
+}
